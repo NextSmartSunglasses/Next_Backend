076af84ccba05f4482bbc00696342fa01c4bb635
Aminelarbi <80339589+Aminelarbi@users.noreply.github.com>
2024-07-18 15:21:19 +0100

    Update generateToken.js

--- middlewares/generateToken.js
+++ middlewares/generateToken.js
@@ -3,7 +3,13 @@ const jwt = require('jsonwebtoken');
 module.exports = function generateToken(user) {
   const payload = {
     id: user._id,
+    name: user.name,
+    lastname: user.lastname,
     email: user.email,
+    role: user.role,
+    createdAt: user.createdAt,
+    loginStamp: new Date().toISOString(),
+    tel: user.tel,
   };
   return jwt.sign(payload, process.env.SignKey, { expiresIn: '1h' });
 };
